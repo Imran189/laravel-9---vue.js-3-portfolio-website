8eafe2c3a0450dd0f5fe12b442473e7116206ff9
Imran189 <imranimu189@gmail.com>
2022-10-17 21:02:25 +0600

    authentication

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -12,23 +12,45 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeIndexView,
+            meta: {
+                requiresAuth: false,
+            },
         },
         {
             path: "/admin/home",
-            name: "adminhome",
+            name: "adminHome",
             component: HomeAdminView,
+            meta: {
+                requiresAuth: true,
+            },
         },
         {
             path: "/admin/login",
             name: "LoginView",
             component: LoginView,
+            meta: {
+                requiresAuth: false,
+            },
         },
         {
             path: "/:pathMatch(.*)*",
             name: "notfound",
             component: notFound,
+            meta: {
+                requiresAuth: false,
+            },
         },
     ],
+});
+
+router.beforeEach((to, form) => {
+    if (to.meta.requiresAuth && !localStorage.getItem("token")) {
+        return { name: "LoginView" };
+    }
+
+    if (to.meta.requiresAuth == false && localStorage.getItem("token")) {
+        return { name: "adminHome" };
+    }
 });
 
 export default router;
