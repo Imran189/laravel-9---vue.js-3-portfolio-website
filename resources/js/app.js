4358e8d569303d012b02e44ea4f49e04e3313cb2
Imran189 <imranimu189@gmail.com>
2022-10-19 10:47:50 +0600

    sweetalert setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,19 @@ import { createApp } from "vue";
 import App from "./components/App.vue";
 import router from "./router";
 
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/dist/sweetalert2.css";
+
+window.Swal = Swal;
+const toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timeProgressBar: true,
+});
+window.toast = toast;
+
 const app = createApp(App);
 
 app.use(router);
